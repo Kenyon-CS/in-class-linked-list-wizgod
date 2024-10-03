fd732bad854ba6728682c99a408f8942846a2fa1
Wisdom Wemoatu Akanwe <151788806+Wiz1702@users.noreply.github.com>
2024-10-03 15:51:24 -0400

    Updated

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -2,70 +2,82 @@
 
 template <typename T>
 class node {
-  public:
-    T data;           // Data of generic type T
-    node* next;       // Pointer to the next node in the list
+public:
+    T data;              // Data of generic type T
+    node* next;          // Pointer to the next node in the list
+    node* prev;          // Pointer to the previous node in the list
 
-    node(T val) : data(val), next(nullptr) {}
+    // Constructor to initialize node with value
+    node(T val) : data(val), next(nullptr), prev(nullptr) {}
 };
 
 template <typename T>
 class LinkedList {
-  private:
+private:
     node<T>* head;  // Pointer to the first node of the list
+    node<T>* tail;  // Pointer to the last node of the list
 
-  public:
-    LinkedList() : head(nullptr) {}
+public:
+    // Constructor: Initializes an empty list
+    LinkedList() : head(nullptr), tail(nullptr) {}
 
-    // Insert at the end
-    void append(T value) {
+    // Insert at the end (push_back)
+    void push_back(T value) {
         node<T>* newNode = new node<T>(value);
-        if (head == nullptr) {
+        if (tail == nullptr) {  // If the list is empty
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Insert at the beginning (push_front)
+    void push_front(T value) {
+        node<T>* newNode = new node<T>(value);
+        if (head == nullptr) {  // If the list is empty
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
             head = newNode;
+        }
+    }
+
+    // Remove from the end (pop_back)
+    void pop_back() {
+        if (tail == nullptr) return;  // List is empty
+        if (head == tail) {  // Only one element in the list
+            delete tail;
+            head = tail = nullptr;
+        } else {
+            node<T>* temp = tail;
+            tail = tail->prev;
+            tail->next = nullptr;
+            delete temp;
+        }
+    }
+
+    // Remove from the beginning (pop_front)
+    void pop_front() {
+        if (head == nullptr) return;  // List is empty
+        if (head == tail) {  // Only one element in the list
+            delete head;
+            head = tail = nullptr;
         } else {
             node<T>* temp = head;
-            while (temp->next != nullptr) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
-
-    // Insert at the beginning
-    void prepend(T value) {
-        node<T>* newNode = new node<T>(value);
-        newNode->next = head;
-        head = newNode;
-    }
-
-    // Remove first occurrence of value
-    void remove(T value) {
-        if (head == nullptr) return;
-
-        if (head->data == value) {
-            node<T>* temp = head;
             head = head->next;
+            head->prev = nullptr;
             delete temp;
-            return;
         }
-
-        node<T>* temp = head;
-        while (temp->next != nullptr && temp->next->data != value) {
-            temp = temp->next;
-        }
-
-        if (temp->next == nullptr) return;
-
-        node<T>* nodeToRemove = temp->next;
-        temp->next = temp->next->next;
-        delete nodeToRemove;
     }
 
     // Print the list
-    void print() {
+    void print() const {
         node<T>* temp = head;
         while (temp != nullptr) {
-            std::cout << temp->data << " -> ";
+            std::cout << temp->data << " <-> ";
             temp = temp->next;
         }
         std::cout << "nullptr" << std::endl;
@@ -84,17 +96,23 @@ class LinkedList {
 int main() {
     LinkedList<int> list;
 
-    list.append(1);
-    list.append(2);
-    list.append(3);
-    list.prepend(0);
+    // Insert values
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_front(0);
 
     std::cout << "Linked List: ";
-    list.print();
+    list.print();  // Expected output: 0 <-> 1 <-> 2 <-> 3 <-> nullptr
 
-    list.remove(2);
-    std::cout << "After removing 2: ";
-    list.print();
+    // Remove from front and back
+    list.pop_front();
+    std::cout << "After pop_front: ";
+    list.print();  // Expected output: 1 <-> 2 <-> 3 <-> nullptr
+
+    list.pop_back();
+    std::cout << "After pop_back: ";
+    list.print();  // Expected output: 1 <-> 2 <-> nullptr
 
     return 0;
 }
